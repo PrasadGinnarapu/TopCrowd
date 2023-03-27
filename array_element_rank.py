556a7eb3d091be5ce34cbebd5d55cc4872d2a73d
prasad-21258 <prasad.ginnarapu@ojas-it.com>
2023-03-27 12:01:50 +0530

    Added two programs

---
+++ array_element_rank.py
@@ -0,0 +1,6 @@
+"""
+Given an array of distinct integers, replace each array element by its corresponding rank in the array.
+The minimum array element has the rank 1; the second minimum element has a rank of 2, and so on… For example,
+Input:  { 10, 8, 15, 12, 6, 20, 1 }
+Output: { 4, 3, 6, 5, 2, 7, 1 }
+"""
---
+++ in_place_merge_arrays.py
@@ -0,0 +1,28 @@
+"""
+25)
+In-place merge two sorted arrays
+Input:
+ 
+X[] = { 1, 4, 7, 8, 10 }
+Y[] = { 2, 3, 9 }
+ 
+Output:
+ 
+X[] = { 1, 2, 3, 4, 7 }
+Y[] = { 8, 9, 10 }
+"""
+
+def in_place_merge(array_1,array_2):
+    """
+    This is function docstring
+    """
+    len_x = len(array_1)
+    array_1.extend(array_2)
+    array_1.sort()
+    return f"X = {[int(i) for i in array_1[:len_x]]} \nY = {[int(i) for i in array_1[len_x:]]}"
+print(in_place_merge(input("Enter array_1: ").split(" "),input("Enter array_2: ").split(" ")))
+
+
+
+
+
